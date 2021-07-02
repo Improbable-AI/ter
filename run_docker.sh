0aded4d6f2fdf186120e32aacdb7abaa5c90bd3a
Zhang-Wei Hong <zwhong@visiongpu50.csail.mit.edu>
2021-07-02 13:09:02 -0400

    update docker files

--- run_docker.sh
+++ run_docker.sh
@@ -1,1 +1,1 @@
-docker run -it --mount type=bind,source=/Users/williamd/Projects/ter-release,target=/app ter bash
+docker run -it --mount type=bind,Source=$(pwd)/,Target=/code,bind-propagation=shared ter bash scripts/minigrid/train_ddqn_er_mixin_ter.sh Empty-5x5
